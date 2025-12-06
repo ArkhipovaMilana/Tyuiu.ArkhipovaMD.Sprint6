af781b89fdb222d228b5a9f99274b3e94aed984c
Milana Arkhipova <Tyuiu.ArkhipovaMD@outlook.com>
2025-12-06 19:17:15 +0500

    Task2

--- Tyuiu.ArkhipovaMD.Sprint6.Task2.V14.Lib/DataService.cs
+++ Tyuiu.ArkhipovaMD.Sprint6.Task2.V14.Lib/DataService.cs
@@ -8,9 +8,17 @@ namespace Tyuiu.ArkhipovaMD.Sprint6.Task2.V14.Lib
             double[] ans = new double[stopValue-startValue+1];
             for (int i = 0; i <= ans.Length-1; i++)
             {
-                double function = 5 - 3 * startValue + (1 + Math.Sin(startValue))/(2*startValue-0.5);
-                ans[i] = function;
-                startValue++;
+                if (2 * startValue - 0.5 == 0)
+                {
+                    ans[i] = 0;
+                    startValue++;
+                }
+                else
+                {
+                    double function = 5 - 3 * startValue + (1 + Math.Sin(startValue)) / (2 * startValue - 0.5);
+                    ans[i] = Math.Round(function,2);
+                    startValue++;
+                }
             }
             return ans;
         }
