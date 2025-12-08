a0beed697d07560c414849476aa042558771f0fc
Milana Arkhipova <Tyuiu.ArkhipovaMD@outlook.com>
2025-12-08 21:46:06 +0500

    Task4

--- Tyuiu.ArkhipovaMD.Sprint6.Task4.V17.Lib/DataService.cs
+++ Tyuiu.ArkhipovaMD.Sprint6.Task4.V17.Lib/DataService.cs
@@ -5,11 +5,13 @@ namespace Tyuiu.ArkhipovaMD.Sprint6.Task4.V17.Lib
     {
         public double[] GetMassFunction(int startValue, int stopValue)
         {
+            //[-0.62,-16.79,-17.77,-6.3,-5.04,-6.0,-7.85,-2.43,5.91,4.33,-11.82]
             int massLen = stopValue - startValue + 1;
             double[] result = new double[massLen];
             for (int i = 0; i < massLen; i++)
             {
-                result[i] = Math.Round(((Math.Sin(startValue)/(startValue+1.7))-Math.Cos(startValue))*(4*startValue)-6,2);
+                double fx = (Math.Sin(startValue) / (startValue + 1.7)) - Math.Cos(startValue) * (4 * startValue) - 6;
+                result[i] = Math.Round(fx,2);
                 startValue++;
             }
             return result;
