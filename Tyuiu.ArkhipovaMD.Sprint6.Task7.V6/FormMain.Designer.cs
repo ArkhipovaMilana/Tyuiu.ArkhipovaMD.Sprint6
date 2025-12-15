b56ddab9853459c4fa6aa08029a7c0360481b1a0
Milana Arkhipova <Tyuiu.ArkhipovaMD@outlook.com>
2025-12-15 21:37:07 +0500

    Task6

--- Tyuiu.ArkhipovaMD.Sprint6.Task7.V6/FormMain.Designer.cs
+++ Tyuiu.ArkhipovaMD.Sprint6.Task7.V6/FormMain.Designer.cs
@@ -28,12 +28,96 @@ namespace Tyuiu.ArkhipovaMD.Sprint6.Task7.V6
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            dataGridViewInput_AMD = new DataGridView();
+            dataGridViewOutput_AMD = new DataGridView();
+            buttonOpen_AMD = new Button();
+            buttonApply_AMD = new Button();
+            buttonSave_AMD = new Button();
+            openFileDialog_AMD = new OpenFileDialog();
+            openFileDialogSave_AMD = new OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewInput_AMD).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewOutput_AMD).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridViewInput_AMD
+            // 
+            dataGridViewInput_AMD.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewInput_AMD.Location = new Point(12, 50);
+            dataGridViewInput_AMD.Name = "dataGridViewInput_AMD";
+            dataGridViewInput_AMD.Size = new Size(342, 388);
+            dataGridViewInput_AMD.TabIndex = 0;
+            // 
+            // dataGridViewOutput_AMD
+            // 
+            dataGridViewOutput_AMD.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewOutput_AMD.Location = new Point(364, 50);
+            dataGridViewOutput_AMD.Name = "dataGridViewOutput_AMD";
+            dataGridViewOutput_AMD.Size = new Size(342, 388);
+            dataGridViewOutput_AMD.TabIndex = 1;
+            // 
+            // buttonOpen_AMD
+            // 
+            buttonOpen_AMD.Location = new Point(12, 12);
+            buttonOpen_AMD.Name = "buttonOpen_AMD";
+            buttonOpen_AMD.Size = new Size(75, 23);
+            buttonOpen_AMD.TabIndex = 2;
+            buttonOpen_AMD.Text = "open";
+            buttonOpen_AMD.UseVisualStyleBackColor = true;
+            buttonOpen_AMD.Click += buttonOpen_AMD_Click;
+            // 
+            // buttonApply_AMD
+            // 
+            buttonApply_AMD.Enabled = false;
+            buttonApply_AMD.Location = new Point(93, 12);
+            buttonApply_AMD.Name = "buttonApply_AMD";
+            buttonApply_AMD.Size = new Size(75, 23);
+            buttonApply_AMD.TabIndex = 3;
+            buttonApply_AMD.Text = "apply";
+            buttonApply_AMD.UseVisualStyleBackColor = true;
+            buttonApply_AMD.Click += buttonApply_AMD_Click;
+            // 
+            // buttonSave_AMD
+            // 
+            buttonSave_AMD.Location = new Point(183, 12);
+            buttonSave_AMD.Name = "buttonSave_AMD";
+            buttonSave_AMD.Size = new Size(75, 23);
+            buttonSave_AMD.TabIndex = 4;
+            buttonSave_AMD.Text = "save";
+            buttonSave_AMD.UseVisualStyleBackColor = true;
+            // 
+            // openFileDialog_AMD
+            // 
+            openFileDialog_AMD.FileName = "openFileDialog1";
+            // 
+            // openFileDialogSave_AMD
+            // 
+            openFileDialogSave_AMD.FileName = "openFileDialog2";
+            // 
+            // FormMain
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(718, 450);
+            Controls.Add(buttonSave_AMD);
+            Controls.Add(buttonApply_AMD);
+            Controls.Add(buttonOpen_AMD);
+            Controls.Add(dataGridViewOutput_AMD);
+            Controls.Add(dataGridViewInput_AMD);
+            Name = "FormMain";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dataGridViewInput_AMD).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewOutput_AMD).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private DataGridView dataGridViewInput_AMD;
+        private DataGridView dataGridViewOutput_AMD;
+        private Button buttonOpen_AMD;
+        private Button buttonApply_AMD;
+        private Button buttonSave_AMD;
+        private OpenFileDialog openFileDialog_AMD;
+        private OpenFileDialog openFileDialogSave_AMD;
     }
 }
--- Tyuiu.ArkhipovaMD.Sprint6.Task7.V6/FormMain.cs
+++ Tyuiu.ArkhipovaMD.Sprint6.Task7.V6/FormMain.cs
@@ -1,3 +1,4 @@
+using Tyuiu.ArkhipovaMD.Sprint6.Task7.V6.Lib;
 namespace Tyuiu.ArkhipovaMD.Sprint6.Task7.V6
 {
     public partial class FormMain : Form
@@ -5,6 +6,69 @@ namespace Tyuiu.ArkhipovaMD.Sprint6.Task7.V6
         public FormMain()
         {
             InitializeComponent();
+            openFileDialogSave_AMD.Filter = "Значения разделённые запятыми(*.csv)|*.csv|Все файлы(*.*)|*.*";
+            openFileDialog_AMD.Filter = "Значения разделённые запятыми(*.csv)|*.csv|Все файлы(*.*)|*.*";
+        }
+        static int rows;
+        static int cols;
+        static string path;
+        DataService ds = new DataService();
+        public static int[,] LoadFromFileData(string path)
+        {
+            string fileData = File.ReadAllText(path);
+
+            fileData = fileData.Replace('\n', '\r');
+            string[] lines = fileData.Split(new char[] { '\r' }, StringSplitOptions.RemoveEmptyEntries);
+            rows = lines.Length;
+            cols = lines[0].Split(';').Length;
+            int[,] arrayValues = new int[rows, cols];
+            for (int r = 0; r < rows; r++)
+            {
+                string[] line_r = lines[r].Split(';');
+                for (int c = 0; c < cols; c++)
+                {
+                    arrayValues[r, c] = Convert.ToInt32(line_r[c]);
+                }
+            }
+            return arrayValues;
+        }
+
+        private void buttonOpen_AMD_Click(object sender, EventArgs e)
+        {
+            openFileDialog_AMD.ShowDialog();
+            path = openFileDialog_AMD.FileName;
+            int[,] arrayValues = new int[rows, cols];
+            arrayValues = LoadFromFileData(path);
+            dataGridViewInput_AMD.RowCount = rows;
+            dataGridViewInput_AMD.ColumnCount = cols;
+            dataGridViewOutput_AMD.RowCount = rows;
+            dataGridViewOutput_AMD.ColumnCount = cols;
+            for (int i = 0; i < cols; i++)
+            {
+                dataGridViewInput_AMD.Columns[i].Width = 25;
+                dataGridViewOutput_AMD.Columns[i].Width = 25;
+            }
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    dataGridViewInput_AMD.Rows[i].Cells[j].Value = arrayValues[i, j];
+                }
+            }
+            arrayValues = ds.GetMatrix(path);
+            buttonApply_AMD.Enabled = true;
+        }
+
+        private void buttonApply_AMD_Click(object sender, EventArgs e)
+        {
+            int[,] arrayValues = ds.GetMatrix(path);
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    dataGridViewOutput_AMD.Rows[i].Cells[j].Value = arrayValues[i, j];
+                }
+            }
         }
     }
 }
